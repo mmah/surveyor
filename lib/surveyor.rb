38319dfd5fa14502b0550798ac3068d512d7cbee
Sunil Pinnamaneni <SunilPinnamaneni@gmail.com>
2013-04-18 14:28:25 -0700

    Updated surveryor to work with rails 3.2.

--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -6,7 +6,7 @@ require 'surveyor/acts_as_response'
 require 'formtastic/surveyor_builder'
 # require 'surveyor/surveyor_controller_methods'
 # require 'surveyor/models/survey_methods'
-Formtastic::SemanticFormHelper.builder = Formtastic::SurveyorBuilder
-Formtastic::SemanticFormBuilder.default_text_area_height = 5
-Formtastic::SemanticFormBuilder.default_text_area_width = 50
-Formtastic::SemanticFormBuilder.all_fields_required_by_default = false
+Formtastic::Helpers::FormHelper.builder = Formtastic::SurveyorBuilder
+Formtastic::FormBuilder.default_text_area_height = 5
+Formtastic::FormBuilder.default_text_area_width = 50
+Formtastic::FormBuilder.all_fields_required_by_default = false
